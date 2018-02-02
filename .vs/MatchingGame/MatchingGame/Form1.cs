84188797fe963918ccb36ce4263569276cd7cc30
Katie <kv4lsu@gmail.com>
2018-02-01 19:25:50 -0500

    old project

---
+++ .vs/MatchingGame/MatchingGame/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!","N","N","G","G","k","k",
+            "2","2","v","v","x","x","z","z"
+        };
+
+        public void AssignIcons()
+        {
+            foreach(Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconsLabel = control as Label;
+                if(iconsLabel != null)
+                {
+                    int rand = random.Next(icons.Count);
+                    iconsLabel.Text = icons[rand];
+                    //iconsLabel.ForeColor = iconsLabel.BackColor;
+                    icons.RemoveAt(rand);
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIcons();
+        }
+    }
+}
---
+++ .vs/MatchingGame/MatchingGameTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MatchingGameTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatchingGameTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97a44a70-610e-46dc-b71a-c99193408934")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
